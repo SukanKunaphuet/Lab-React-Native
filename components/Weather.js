28ec43afa60f4840f9de485ad5a8f9a55e5ffdb4
SukanKunaphuet <6310110518@email.psu.ac.th>
2022-08-25 14:00:30 +0700

    commit part 1

---
+++ components/Weather.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import { Text, StyleSheet, ImageBackground, View } from "react-native";
+import Forecast from "./Forecast";
+
+export default function Weather(props) {
+    const [forecastInfo, setForecastInfo] = useState({
+        main: '-',
+        description: '-',
+        temp: 0
+    })
+    return (
+        <View>
+            <ImageBackground source={require('../7f631e577ed5e5ffbbce726f8ec03489.jpg')} style={StyleSheet.backdrop}>
+                <Text>Zip Code</Text>
+                <Text>{props.zipCode}</Text>
+                <Forecast {...forecastInfo} />
+            </ImageBackground>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    backdrop: {
+        alignItems: 'center',
+        width: '100%',
+        height: '100%'
+    },
+});
